3452ae3d7c91c80d6dee3302caba44b4ed89df2f
Mike Wang <mikewang1@icloud.com>
2024-05-14 20:03:36 +0200

    implemented most coefficients

--- main.py
+++ main.py
@@ -2,4 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
-print("kaolo deze")
+# Equation implemented from Chaigne and Askenfeld 1993 
+# Define constants 
+
+eps = 1.0 # string stiffness parameter
+b_1 = 1.0 # damping coefficient 
+b_3 = 1.0 # damping coefficient 
+T = 1.0 # string tension 
+mu = 1.0 # linear mass density of string 
+c = np.sqrt(T/mu)
+
+N = 1.0e3 # no. of string segments 
+dx = 0.001 
+dt = 0.001
+
+D = 1 + b_1 * dt + 2 * b_3 / dt 
+r = c * dt / dx
+
+a_1 = (2 - 2*r**2 + b_3/dt - 6 * eps * N**2 * r**2)/D
+a_2 = (-1 + b_1 * dt + 2*b3/dt)/D
+a_3 = (r**2 * (1 + 4 * eps * N**2))/D
+a_4 = (b_3/dt - eps * N**2 * r**2)/D
+a_5 = (-b_3 / dt) / D
+
+
+
+#Implement FDM here 
+
+
+def 
---
+++ test.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+# Based on the MUS420 Modal Synthesis of a Piano String 
+
+
