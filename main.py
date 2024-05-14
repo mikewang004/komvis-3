476d1db8d1adc6bd0d83c41f73dcb7fb18f9961c
Mike Wang <mikewang1@icloud.com>
2024-05-14 20:27:28 +0200

    test

--- main.py
+++ main.py
@@ -12,22 +12,27 @@ T = 1.0 # string tension
 mu = 1.0 # linear mass density of string 
 c = np.sqrt(T/mu)
 
-N = 1.0e3 # no. of string segments 
+N = 100 # no. of string segments 
 dx = 0.001 
 dt = 0.001
+max_t = 100
 
 D = 1 + b_1 * dt + 2 * b_3 / dt 
 r = c * dt / dx
 
 a_1 = (2 - 2*r**2 + b_3/dt - 6 * eps * N**2 * r**2)/D
-a_2 = (-1 + b_1 * dt + 2*b3/dt)/D
+a_2 = (-1 + b_1 * dt + 2*b_3/dt)/D
 a_3 = (r**2 * (1 + 4 * eps * N**2))/D
 a_4 = (b_3/dt - eps * N**2 * r**2)/D
 a_5 = (-b_3 / dt) / D
 
 
 
-#Implement FDM here 
+# Implement FDM here 
+
+A = np.zeros([N, max_t])
+
+# Define boundary conditions 
+A[0, 0] = 0; A[N-1, max_t-1] = 0
 
 
-def 
