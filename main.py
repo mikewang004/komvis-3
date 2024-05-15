e66e0ad54e8f804318381c7110a89cba0e93eaf1
Mike Wang <mikewang1@icloud.com>
2024-05-15 15:29:18 +0200

    test

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ mu = 1.0 # linear mass density of string
 c = np.sqrt(T/mu)
 
 N = 100 # no. of string segments 
-dx = 0.001 
-dt = 0.001
-max_t = 100
+dx = 0.01 
+dt = 0.01
+max_t = 1
+fund_freq = 440 # Hz, assume standard tuning frequency A4 
+samp_freq = 32000 # Hz
 
 D = 1 + b_1 * dt + 2 * b_3 / dt 
 r = c * dt / dx
@@ -27,12 +29,23 @@ a_4 = (b_3/dt - eps * N**2 * r**2)/D
 a_5 = (-b_3 / dt) / D
 
 
+def construct_solution_matrix(N, max_t):
+    # Implement FDM here 
+    end_begin_conditions = 3 # index
+    A = np.zeros([N, int(max_t/dt)])
+    A[0, 0] = 0; A[N-1, max_t-1] = 0 # Define boundary conditions 
+    A[:end_begin_conditions, :end_begin_conditions] = 0 # Implement easiest solution that first 3 timesteps are silent 
+    for n in range(end_begin_conditions, int(max_t/dt)-1): # time index
+        for i in range(end_begin_conditions-1, N-2): # place index 
+            A[i, n + 1] = a_1 * A[i, n] + a_2 * A[i, n-1] + \
+            a_3 * (A[i+1, n] + A[i -1, n]) + a_4 * (A[i + 2, n] + A[i - 2, n]) + \
+            a_5 * (A[i+1, n-1] + A[i -1, n-1,] + A[i, n - 2])
+    return A
 
-# Implement FDM here 
+A = construct_solution_matrix(N, max_t)
+print(A)
 
-A = np.zeros([N, max_t])
 
-# Define boundary conditions 
-A[0, 0] = 0; A[N-1, max_t-1] = 0
+
 
 
