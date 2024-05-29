4c253234e3b51d73af7eb959b42fd8260bc62c57
viperfish-oc <71723888+viperfish-oc@users.noreply.github.com>
2024-05-29 05:17:40 +0200

    made branch for this attempt

--- main2.py
+++ main2.py
@@ -30,11 +30,11 @@ hammer_striking_position = 0.12
 hammer_location_index = int(
     hammer_striking_position * n_physical_segments + 1
 )  # add one due to the leftmost virtual point
-eta_zeroth_step = 10.5  # position of the hammer at t=0 random value??????????
+eta_zeroth_step = 1.5  # position of the hammer at t=0 random value??????????
 
 dx = L / n_physical_segments
-dt = 0.0001
-max_t = 1.0
+dt = 1e-6
+max_t = 100 * dt
 
 
 samp_freq = 32000  # Hz
@@ -74,6 +74,7 @@ n_total_segments = n_physical_segments + n_virtual_points
 
 string_position_arr = np.zeros([n_steps, n_total_segments])
 eta = np.zeros(n_steps)
+eta_derivative = np.zeros(n_steps)
 F_h = np.zeros(n_steps)
 
 
@@ -81,8 +82,8 @@ F_h = np.zeros(n_steps)
 window_function = np.zeros(n_total_segments)
 window_function[hammer_location_index - 2 : hammer_location_index + 1] = [1.5, 0, 1.5]
 
-
 # initialize by doing 3 steps
+
 # first, make an estimate for the first step
 zeroth_step = np.zeros(n_total_segments)
 first_step = 0.5 * (np.roll(zeroth_step, 1) + np.roll(zeroth_step, -1))
@@ -111,13 +112,22 @@ string_position_arr[0, :] = zeroth_step
 string_position_arr[1, :] = first_step
 string_position_arr[2, :] = second_step
 
+
+eta[0] = eta_zeroth_step
+eta[1] = eta_first_step
+eta[2] = eta_second_step
+
 ############
 
-start_index = 3  # start after the initialization
+start_index = 2  # start after the initialization steps
 for i in range(start_index, n_steps - 1):
     two_steps_earlier = string_position_arr[i - 2, :]
     one_step_earlier = string_position_arr[i - 1, :]
     current = string_position_arr[i, :]
+    eta_current = eta[i]
+    eta_derivative_current = eta[i]
+    force_magnitude_current = hammer_force(eta_current, current[hammer_location_index])
+    print(force_magnitude_current)
 
     next_step = (
         a_1 * current
@@ -130,12 +140,24 @@ for i in range(start_index, n_steps - 1):
             + np.roll(one_step_earlier, -1)
             + two_steps_earlier
         )
-        + 0  # force term
+        + (
+            dt**2
+            * n_physical_segments
+            * force_magnitude_current
+            * window_function[hammer_location_index]
+        )
+        / M_s
     )
+
+    # use euler forward to integrate the hammer. euler forward in 2 steps: 1 for derivative and 1 for second derivative.
+    eta_derivative[i + 1] = (
+        eta_derivative_current
+        + dt * -1 * hammer_force(eta_current, current[hammer_location_index]) / M_h
+    )
+    eta[i + 1] = dt * eta_derivative_current
 
     # the duplicate points mirror the points right before the boundry
     update_virtual_points(next_step)
-
     string_position_arr[i + 1, :] = next_step
 
 
@@ -153,7 +175,6 @@ def make_animation(A):
     x = np.arange(0, n_total_segments)
 
     def plot_A(frame):
-        print(frame)
         frame = frame
         line.set_data(x, A[int(frame), :])
         return A[int(frame), :]
@@ -162,4 +183,35 @@ def make_animation(A):
     plt.show()
 
 
-make_animation(string_position_arr)
+def make_double_animation(A, B):
+    n_frames = np.shape(A)[0]
+    n_total_segments = np.shape(A)[1]
+    fig = plt.figure()
+    padding = 2
+    ax = plt.axes(
+        xlim=(0 - padding, n_total_segments + padding),
+        # ylim=(-10e12, 10e12)
+    )
+
+    (line,) = ax.plot([], [], lw=3)
+    (line2,) = ax.plot([], [], lw=3, marker="x")
+    x = np.arange(0, n_total_segments)
+
+    def plot_A(frame):
+        frame = frame
+        line.set_data(x, A[int(frame), :])
+        hammer_dummy_location = 4
+        line2.set_data(hammer_dummy_location, B[int(frame)])
+        return A[int(frame), :]
+
+    ani = animation.FuncAnimation(fig, plot_A, frames=n_frames, interval=1)
+    plt.show()
+
+
+make_double_animation(string_position_arr, eta)
+
+print(eta)
+
+
+plt.plot(eta)
+plt.show()
--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Based on the MUS420 Modal Synthesis of a Piano String 
+# Based on the MUS420 Modal Synthesis of a Piano String
 
 
+A = np.array([1, 2, 3, 4, 5])
+
+a = 2
+print(A[a - 1 : a + 1])
+print(A[-3])
